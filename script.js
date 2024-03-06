eeebdd01593f0669d367a17af72b3ff5879c047b
gtracey24 <146043771+cptnez@users.noreply.github.com>
2024-03-06 10:35:55 -0600

    Added alert for form

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  //INPUTS
+  // let inputPilotName = document.getElementById('pilotName');
+  // let inputCopilotName = document.getElementById('copilotName');
+  // let inputFuelLevel = document.getElementById('fuelLevel');
+  // let inputCargoMass = document.getElementById('cargoMass');
+  // let inputAll = [inputPilotName, inputCopilotName, inputFuelLevel, inputCargoMass]
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    let inputPilotName = document.querySelector("input[name=pilotName]");
+    let inputCopilotName = document.querySelector("input[name=copilotName");
+    let inputFuelLevel = document.querySelector("input[name=fuelLevel");
+    let inputCargoMass = document.querySelector("input[name=cargoMass");
+
+    if (
+      inputPilotName.value === "" ||
+      inputCopilotName.value === "" ||
+      inputFuelLevel.value === "" ||
+      inputCargoMass.value === ""
+    ) {
+      alert("All fields are required!");
+      event.preventDefault();
+    }
+  });
+
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse;
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
     })
-    
- });
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
+});
