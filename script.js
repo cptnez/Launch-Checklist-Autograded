477d979c34292565a0a9b0822f227489abab72e4
Grant Tracey <gtracey24@gmail.com>
2024-03-12 13:30:50 -0500

    Restored script file from last commit. 7 tests passing and oiperating correctly. Still no validation

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 // Write your JavaScript code here!
 
-const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
-
 // const { validateInput, formSubmission } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
   let form = document.querySelector('form');
   let button = document.getElementById('formSubmit')
+ 
+  let inputPilot = document.querySelector("input[name=pilotName]");
+  let inputCopilot = document.querySelector("input[name=copilotName]");
+  let inputFuelLevel = document.querySelector("input[name=fuelLevel]");
+  let inputCargoMass = document.querySelector("input[name=cargoMass]");
+ 
 
   form.addEventListener("submit", function(event) {
 
-    
+
     event.preventDefault();
-    
+    // let document = document;
     let list = document.getElementById('faultyItems').value;
     let pilot = document.querySelector("input[name=pilotName]").value;
     let copilot = document.querySelector("input[name=copilotName]").value;
     let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
     let cargoMass = document.querySelector("input[name=cargoMass]").value;
 
-    formSubmission(
+    if (
+      pilot.value === "" ||
+      copilot.value === "" ||
+      fuelLevel.value === "" ||
+      cargoMass.value === ""
+    ) {
+      alert("All fields are required!");
+      event.preventDefault();
+    }
+
+   if (formSubmission(
       document,
       list,
       pilot,
       copilot,
       fuelLevel,
       cargoMass
-    );
+    ));
+
     
   });
 
@@ -39,16 +54,7 @@ window.addEventListener("load", function() {
       console.log(listedPlanets);
   }).then(function () {
       console.log(listedPlanets);
-      // Below this comment call the appropriate helper functions to
-      //pick a planet fom the list of planets and add that information to your destination.
-      planet = pickPlanet(listedPlanets);
-      addDestinationInfo(document.getElementById('missionTarget'),
-        planet.name,
-        planet.diameter,
-        planet.star,
-        planet.distance,
-        planet.moons,
-        planet.imageUrl)
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
   })
   
 });
